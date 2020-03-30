38aad9763143612ec6ddabb9c6b98aa03cfea4c9
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2020-03-29 22:19:21 -0400

    Add files via upload

--- PetClasses.py
+++ PetClasses.py
@@ -1,10 +1,38 @@
-class MonsterPet:
-    def __init__(self, name, health, attack, defense, special, speed):
+class Pypet:
+    def __init__(self, name: object, level: object, health: object, attack: object, defense: object, special: object, speed: object, dexterity: object) -> object:
         self.name = name
+        self.level = level
         self.health = health
         self.attack = attack
         self.defense = defense
         self.special = special
         self.speed = speed
+        self.dexterity = dexterity
 
-#       def __
+    def battle(self):
+        MonsterPet() versus MonsterPet()
+
+    def attack:
+        self.attack * self.level / 2
+
+    def defense:
+        self.defense * level / 2
+
+    def damage:
+        dmg = health - (target.attack / self.defense)
+
+    def faint:
+        when self.health = 0
+
+
+
+
+Cat = Pypet("Kitty", 1 9, 4, 4, 4, 10, 10)
+
+Dog = Pypet("Puppy", 1 10, 5, 5, 5, 9, 5)
+
+Parrot = Pypet("Birdy", 1, 8, 3, 6, 9, 9, 3)
+
+
+
+
